9aa90988b94c5c22bb1f037580c8c8a0dea2a2ce
William Moura <williamonte@gmail.com>
2024-08-31 21:50:38 -0300

    Exercício 04

---
+++ ex_04_Iterando_Arrays_de_Objetos.js
@@ -0,0 +1,26 @@
+/*4. Iterando Sobre Arrays de Objetos
+- Objetivo: Crie um array de objetos pessoas, onde cada objeto representa
+uma pessoa com nome, idade, e cidade. Use for of para exibir as
+informações de cada pessoa no console.*/
+
+const peopleArrayObjects = [
+    {
+        name: "William",
+        age: "32",
+        city: "Florianópolis"
+    },
+    {
+        name: "Caroline",
+        age: "31",
+        city: "Florianópolis"
+    },
+    {
+        name: "Andressa",
+        age: "22",
+        city: "Santa Maria"
+    }
+]
+
+for(let person of peopleArrayObjects){
+    console.log(`Name: ${person.name}, Age: ${person.age}, City: ${person.city}`);
+}
