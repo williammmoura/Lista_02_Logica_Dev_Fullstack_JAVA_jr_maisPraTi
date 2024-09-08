b5f64aef3b61b27bbad825b6f4a9fc665748f312
William Moura <williamonte@gmail.com>
2024-09-08 20:01:13 -0300

    Exercício 09

---
+++ ex_09_contabiliando_elementos_com_uma_condicao.js
@@ -0,0 +1,30 @@
+/*9. Contabilizando Elementos com uma Condição
+- Objetivo: Crie um array de objetos clientes, cada um com propriedades
+nome, idade, e cidade. Use forEach para contar quantos clientes têm mais
+de 30 anos*/
+
+const customersArrayOfObjects = [
+    {
+        name: "William",
+        age: 32,
+        city: "Florianópolis"
+    },
+    {
+        name: "Caroline",
+        age: 31,
+        city: "Florianópolis"
+    },
+    {
+        name: "Maggie",
+        age: 12,
+        city: "Florianópolis"
+    }
+];
+
+console.log('Customers over 30 years old.');
+
+customersArrayOfObjects.forEach(customers => {
+    if(customers.age > 30){
+        console.log(`Name: ${customers.name}`);
+    }
+})
