be24292575ab02e895de5efc2d3bf331fb3b7bc2
William Moura <williamonte@gmail.com>
2024-09-08 22:47:48 -0300

    Exercício 11

---
+++ ex_11_agrupando_elementos_com_for_each.js
@@ -0,0 +1,37 @@
+/*11. Agrupando Elementos com forEach
+- Objetivo: Crie um array de objetos pedidos, onde cada pedido tem
+cliente, produto, e quantidade. Use forEach para criar um objeto que
+agrupa a quantidade total de produtos por cliente.
+*/
+
+const requestsArrayOfObjects = [
+    {
+        customer: "William",
+        product: "SF22 - 1:43 - Burago",
+        quantity: 2
+    },
+    {
+        customer: "Caroline",
+        product: "Perfume Benetton",
+        quantity: 1
+    },
+    {
+        customer: "Maggie",
+        product: "Ração Prime Sênior",
+        quantity: 3
+    }
+];
+
+let totalRequestsByCustomer = {};
+
+
+requestsArrayOfObjects.forEach(request => {
+    // Verifica se o cliente já existe no objeto, se não, inicia com 0
+    if (!totalRequestsByCustomer[request.customer]) {
+        totalRequestsByCustomer[request.customer] = 0;
+    }
+    // Soma a quantidade de produtos para cada cliente
+    totalRequestsByCustomer[request.customer] += request.quantity;
+});
+
+console.log(totalRequestsByCustomer);
