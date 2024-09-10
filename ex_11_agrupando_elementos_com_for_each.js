6d6c350634faf9bd38049b05035cd919aa4a3e28
William Moura <williamonte@gmail.com>
2024-09-09 22:09:23 -0300

    Feat: Exercício 11

--- ex_11_agrupando_elementos_com_for_each.js
+++ ex_11_agrupando_elementos_com_for_each.js
@@ -1,6 +1,6 @@
 /*11. Agrupando Elementos com forEach
 - Objetivo: Crie um array de objetos pedidos, onde cada pedido tem
-cliente, produto, e quantidade. Use forEach para criar um objeto que
+cliente, produto, e quantidade. Use "forEach" para criar um objeto que
 agrupa a quantidade total de produtos por cliente.
 */
 
